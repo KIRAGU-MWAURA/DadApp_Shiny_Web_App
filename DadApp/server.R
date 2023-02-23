931e7707b1f36e97275564d397653a24cc4f4318
Kiragu <44839744+KIRAGU-MWAURA@users.noreply.github.com>
2023-02-23 10:39:22 +0300

    Delete server.R

--- DadApp/server.R
+++
@@ -1,9 +0,0 @@
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    # draw the histogram with the specified number of bins
-    hist(rnorm(n = 1000,mean = input$Mean), breaks = 100, col = 'darkgray', border = 'white')
-  })
-}
